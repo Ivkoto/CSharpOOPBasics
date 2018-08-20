4e8e202de4a00afac1971d27155d1df42767ba02
Ivaylo Kostov <ikostov87@gmail.com>
2018-08-21 02:16:04 +0300

    Added bank account class

--- BankAccount2/BankAccount.cs
+++ BankAccount2/BankAccount.cs
@@ -6,6 +6,18 @@ namespace BankAccount2
 {
     class BankAccount
     {
+        private int id;
 
+        private double balance;
+
+        public BankAccount()
+        {
+            this.Id = id;
+            this.Balance = balance;
+        }
+
+        public int Id { get; set; }
+
+        public double Balance { get; set; }
     }
 }
